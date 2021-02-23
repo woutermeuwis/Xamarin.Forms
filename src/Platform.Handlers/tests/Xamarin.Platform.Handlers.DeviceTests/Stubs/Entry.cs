4067a547ec3ebce670f9b93839ff05146a4c2a02
Shane Neuville <shneuvil@microsoft.com>
2021-02-22 18:23:55 -0600

    - fix entry

--- src/Platform.Handlers/tests/Xamarin.Platform.Handlers.DeviceTests/Stubs/Entry.cs
+++ src/Platform.Handlers/tests/Xamarin.Platform.Handlers.DeviceTests/Stubs/Entry.cs
@@ -9,5 +9,11 @@ namespace Xamarin.Platform.Handlers.DeviceTests.Stubs
 		public Color TextColor { get; set; }
 
 		public bool IsPassword { get; set; }
+
+		public FontAttributes FontAttributes { get; set; }
+
+		public string FontFamily { get; set; }
+
+		public double FontSize { get; set; }
 	}
 }
